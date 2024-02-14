af1edd5514be162f8dc637ced8e90695584807dd
Vivek Garg <vivek.garg21@gmail.com>
2024-02-14 16:32:07 +0530

    lenovobranch

--- src/main/java/pwj/qa/ecomm/pages/ProductDetailPage.java
+++ src/main/java/pwj/qa/ecomm/pages/ProductDetailPage.java
@@ -9,6 +9,7 @@ public class ProductDetailPage {
 	private String loc_proddetailSec = "//div[@id='content']//div[@class='col-sm-4']";
 	private String loc_prodnameTxt = loc_proddetailSec+"//h1";
 	private String loc_prodbrandTxt = loc_proddetailSec+ "//ul[1]//li[1]";
+	private String loc_productpriceTxt = loc_proddetailSec+ "//ul[2]//h2";
 	
 	protected PlaywrightFactory pf;
 	
